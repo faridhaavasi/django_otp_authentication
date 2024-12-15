7c0b9ef7bff857f8e12a44054088c38999175242
faridhaavasi <faridhavasi777@gmail.com>
2024-12-15 21:40:54 +0330

    reeor user model bug=1.0

--- apps/authentication/v1/urls.py
+++ apps/authentication/v1/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from apps.authentication.v1.views.sigin import MobileSetApiView
+
+urlpatterns = [
+    path('setmobileApi', MobileSetApiView.as_view(), name='setmobileApi'),
+
+]
 
 
--- apps/authentication/v1/views/sigin.py
+++ apps/authentication/v1/views/sigin.py
@@ -5,15 +5,37 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import status
 import random
 import threading
+import uuid
 from apps.authentication.models import OTP
-from apps.user.models import CustomUser as user
+from apps.user.models import CustomUser as User
 from apps.authentication.v1.serializers.sigin_serializers import (
     MobileRegisterSerializer, 
     MobileSetSerializer, 
     VerifyOTPSerializer,
     SetInformationSerializer,
     )
+def send_sms_otp(mobile: str, otp: str):
+    '''
+        send sms otp in mobile number by kavenegar
+    '''
+    return f'send {otp} in {mobile}'
 
 
-
-
+class MobileSetApiView(GenericAPIView):
+    serializer_class = MobileSetSerializer
+    def post(self, request):
+        seriaizer = self.serializer_class(data=request.data)
+        if seriaizer.is_valid():
+            mobile = seriaizer.validated_data['mobile']
+            user_mobile = User.objects.filter(mobile=mobile).exists() # bool
+            if user_mobile:
+                return Response({'meesage': 'You have already registered, please login.'})
+            else:
+                user = User(mobile=mobile)
+                user.save()
+                token = uuid()
+                otp_send = random.randint(100000, 999999)
+                OTP.objects.create(token=token,mobile=mobile, code=otp_send)
+                threading.Thread(send_sms_otp,args=(mobile, otp_send)).start()
+                return Response({'token': token, 'otp': otp_send})
+        return Response(seriaizer.errors)
