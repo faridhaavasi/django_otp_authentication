93a5879d91a796f0d7ab0ff91e180c6879b25f89
faridhaavasi <faridhavasi777@gmail.com>
2024-12-16 18:54:07 +0330

    debug error superuser password and mobile

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('mobile', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('mobile', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_verify', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('mobile', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser')
+        }),
+    )
+    search_fields = ('mobile', 'email')
+    ordering = ('mobile',)
 
-# Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,10 +1,12 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, mobile, password=None, **extra_fields):
         if not mobile:
             raise ValueError("The Mobile field must be set")
+        # Set default values for required fields
+        extra_fields.setdefault('is_active', True)
         user = self.model(mobile=mobile, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -13,15 +15,20 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, mobile, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
         return self.create_user(mobile, password, **extra_fields)
 
-
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     mobile = models.CharField(max_length=11, unique=True)
     email = models.EmailField(unique=True, blank=True, null=True)
     first_name = models.CharField(max_length=25, blank=True, null=True)
     last_name = models.CharField(max_length=25, blank=True, null=True)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_verify = models.BooleanField(default=False, null=True, blank=True)
@@ -31,6 +38,3 @@ class CustomUser(AbstractBaseUser):
 
     USERNAME_FIELD = 'mobile'
     REQUIRED_FIELDS = []
-
-    def __str__(self):
-        return self.mobile
