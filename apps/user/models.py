5b8146ec0777b2fbe2d59ede5060090e5f41b600
faridhaavasi <faridhavasi777@gmail.com>
2024-12-16 18:04:26 +0330

    debug error usermoodel =1.0

--- apps/user/models.py
+++ apps/user/models.py
@@ -23,6 +23,7 @@ class CustomUser(AbstractBaseUser):
     last_name = models.CharField(max_length=25, blank=True, null=True)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     is_verify = models.BooleanField(default=False, null=True, blank=True)
     date_joined = models.DateTimeField(auto_now_add=True)
 
