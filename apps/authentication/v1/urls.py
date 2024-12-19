bfee19d8d76688501d577be013c09bfdb0332068
faridhaavasi <faridhavasi777@gmail.com>
2024-12-19 18:24:17 +0330

    bug with setmobile

--- apps/authentication/v1/urls.py
+++ apps/authentication/v1/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.authentication.v1.views.sigin import MobileSetApiView
+from apps.authentication.v1.views.sigin import MobileSetApiView, SetOtpCode
 
 urlpatterns = [
     path('setmobileApi', MobileSetApiView.as_view(), name='setmobileApi'),
+    path('setotpApi', SetOtpCode.as_view(), name='setotpApi'),
 
 ]
 
--- apps/authentication/v1/views/sigin.py
+++ apps/authentication/v1/views/sigin.py
@@ -33,9 +33,23 @@ class MobileSetApiView(GenericAPIView):
             else:
                 user = User(mobile=mobile)
                 user.save()
-                token = uuid()
+                token = str(uuid.uuid4())
                 otp_send = random.randint(100000, 999999)
                 OTP.objects.create(token=token,mobile=mobile, code=otp_send)
                 threading.Thread(send_sms_otp,args=(mobile, otp_send)).start()
                 return Response({'token': token, 'otp': otp_send})
         return Response(seriaizer.errors)
+
+class SetOtpCode(GenericAPIView):
+    serializer_class = VerifyOTPSerializer
+    def post(self, request):
+        seriaizer = self.serializer_class(data=request.data)
+        if seriaizer.is_valid():
+            token = seriaizer.validated_data['token']
+            code = seriaizer.validated_data['code']
+            otp = OTP.objects.filter(token=token, code=code).exists()
+            if otp:
+                return Response({'message': 'otp is valid'})
+            return Response({'message': 'otp is invalid'})
+        return Response(seriaizer.errors)
+    
