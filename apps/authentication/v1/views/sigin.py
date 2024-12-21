6677e83c518d6defecfcd7f46c372e80bf6dda87
faridhaavasi <faridhavasi777@gmail.com>
2024-12-21 15:37:15 +0330

    review code

--- apps/authentication/v1/views/sigin.py
+++ apps/authentication/v1/views/sigin.py
@@ -36,13 +36,12 @@ class MobileSetApiView(GenericAPIView):
             if recent_otp:
                 return Response({'message': 'Please wait before requesting another OTP'}, status=status.HTTP_429_TOO_MANY_REQUESTS)
 
-            user = User(mobile=mobile)
-            user.save()
             token = str(uuid.uuid4())
             otp_send = random.randint(100000, 999999)
             OTP.objects.create(token=token, mobile=mobile, code=otp_send)
 
             threading.Thread(target=send_sms_otp, args=(mobile, otp_send)).start()
+            User.objects.get_or_create(mobile=mobile)
 
             return Response({'token': token, 'message': 'OTP sent successfully. Please verify.'}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -56,12 +55,13 @@ class SetOtpCode(GenericAPIView):
             token = serializer.validated_data['token']
             code = serializer.validated_data['code']
             mobile = OTP.objects.get(token=token).mobile
-            refesh = RefreshToken.for_user(User.objects.get(mobile=mobile))
-            access_token = str(refesh.access_token)
-            otp = OTP.objects.filter(token=token, code=code).first()
+            refresh = RefreshToken.for_user(User.objects.get(mobile=mobile))
+            
+
+        
+            otp = OTP.objects.filter(token=token, code=code).exists()
             if otp:
-                otp.delete()
-                return Response({'message': 'OTP verified successfully', 'token': access_token}, status=status.HTTP_200_OK)
+                return Response({'message': 'OTP verified successfully', 'token': str(refresh)}, status=status.HTTP_200_OK)
             return Response({'message': 'Invalid OTP'}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -72,13 +72,13 @@ class SetInformationApiView(GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             token = serializer.validated_data['token']
-            user = User.objects.get(mobile=OTP.objects.get(token=token).mobile)
-            user.email = serializer.validated_data['email']
+            mobile = RefreshToken(token).get('mobile')
+            user = User.objects.get(mobile=mobile)
             user.first_name = serializer.validated_data['first_name']
             user.last_name = serializer.validated_data['last_name']
+            user.email = serializer.validated_data['email']
             user.set_password(serializer.validated_data['password'])
-            user.is_active = True
-            user.is_verify = True
             user.save()
+         
             return Response({'message': 'password set successfully'}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
